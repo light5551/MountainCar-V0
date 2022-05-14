a0a22f5a3240fc21265920f06da62e5d1fb270c5
AndrewGavril <gavrilov.andrew1999@yandex.ru>
2022-05-14 13:27:57 +0300

    Environment with keyboard control

---
+++ main.py
@@ -0,0 +1,42 @@
+import gym
+import numpy as np
+import pygame
+env = gym.make("MountainCar-v0")
+env.action_space.seed(42)
+
+
+
+observation, info = env.reset(seed=42, return_info=True)
+stack = [1] # accelerate
+
+def update():
+    events = pygame.event.get()
+
+    for event in events:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                stack.append(0)
+            if event.key == pygame.K_RIGHT:
+                stack.append(2)
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                stack.remove(0)
+            if event.key == pygame.K_RIGHT:
+                stack.remove(2)
+
+
+    action = stack[-1]
+    print(action)
+    observation, reward, done, info = env.step(action)
+    env.render()
+    if done:
+        observation, info = env.reset(return_info=True)
+
+
+env.render()
+for _ in range(1000):
+    update()
+
+print('quit')
+
+env.close()
